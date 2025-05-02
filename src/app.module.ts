0039eafe0c898fa4dd7715bff25b48424da66975
adnanhjoy <engr.adnanhossain@gmail.com>
2025-05-02 19:04:02 +0600

    add user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TestController } from './test/test.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TestService } from './test/test.service';
 import { TestModule } from './test/test.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test-nest'), TestModule
   ],
-  controllers: [AppController, TestController],
-  providers: [AppService],
+  controllers: [AppController, TestController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule { }
