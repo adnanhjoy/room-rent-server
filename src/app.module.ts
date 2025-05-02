b716243c7755dff49c18abb74a90bd4c2b9c056f
adnanhjoy <engr.adnanhossain@gmail.com>
2025-05-02 19:41:42 +0600

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,15 @@ import { TestService } from './test/test.service';
 import { TestModule } from './test/test.module';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test-nest'), TestModule
+    MongooseModule.forRoot('mongodb://localhost:27017/test-nest'), TestModule, AuthModule
   ],
-  controllers: [AppController, TestController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController, TestController, UserController, AuthController],
+  providers: [AppService, UserService, AuthService],
 })
 export class AppModule { }
--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,24 +1,21 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-    @Prop({
-        required: true
-    })
-    name: string;
-    @Prop({
-        required: true
-    })
-    email: string;
-    @Prop({
-        required: true
-    })
-    password: string;
-    @Prop()
-    age: number;
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  age: number;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
