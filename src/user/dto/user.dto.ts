3e7324ebca3e69894663fe9a6788d2e7f9041e34
aotrek <otatbpdevelopment@gmail.com>
2025-05-04 17:16:18 +0600

    [ADD] - added swagger added description and modify

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,38 +1,43 @@
+import { ApiHideProperty, ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
-export class CreateUserDto{
+export class CreateUserDto {
+    @ApiProperty({ required: true, description: "Name" })
     @IsString()
     @IsNotEmpty()
     name: string;
 
-
+    @ApiProperty({ required: true, description: "email" })
     @IsString()
     @IsNotEmpty()
     email: string;
 
-
+    @ApiProperty({ required: true, description: "password" })
     @IsString()
     @IsNotEmpty()
     password: string;
 
-
+    @ApiProperty({ required: true, description: "role", enum: ['Admin', 'Owner', 'User'], default: "user" })
     @IsString()
     @IsNotEmpty()
     role: string;
 
-
+    @ApiProperty({ required: false, description: "age" })
     @IsOptional()
     @IsNumber()
     age?: number;
 }
 
 
-export class LoginUserDto{
+export class LoginUserDto {
+    @ApiProperty({ required: true, description: "email" })
+    // @ApiHideProperty()
     @IsString()
     @IsNotEmpty()
     email: string;
 
-
+    // @ApiHideProperty()
+    @ApiProperty({ required: true, description: "password" })
     @IsString()
     @IsNotEmpty()
     password: string;
