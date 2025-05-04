e42c45f46a86fae5ed887e2bc971c48f6cf2a8c8
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-05-04 16:48:50 +0600

    add login controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from 'src/user/dto/user.dto';
+import { CreateUserDto, LoginUserDto } from 'src/user/dto/user.dto';
 import { Response } from 'express';
 import { sendResponse } from 'src/utils/sendResponse';
 
@@ -11,11 +11,26 @@ export class AuthController {
     @Post('/create')
     async userCreate(@Body() payload: CreateUserDto, @Res() res: Response) {
         const result = await this.authService.createUser(payload);
-        
+
         sendResponse(res, {
             success: true,
             statusCode: HttpStatus.CREATED,
             message: "User created successfull",
+            data: result
+        })
+    }
+
+
+
+
+    @Post('/login')
+    async userLogin(@Body() payload: LoginUserDto, @Res() res: Response) {
+        const result = await this.authService.userLogin(payload);
+
+        sendResponse(res, {
+            success: true,
+            statusCode: HttpStatus.CREATED,
+            message: "Login Successfull",
             data: result
         })
     }
