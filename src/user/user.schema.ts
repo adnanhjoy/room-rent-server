0cd2a2b2542dab7a065cb9cc77e5e6fa4b2baabd
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-05-04 16:28:56 +0600

    update auth service and validate user create controller

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -8,11 +8,26 @@ export class User {
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({
+    required: true,
+    unique: true,
+    type: String
+  })
   email: string;
 
-  @Prop({ required: true })
+  @Prop({
+    required: true,
+    type: String,
+    minlength: 8
+  })
   password: string;
+
+  @Prop({
+    type: String,
+    enum: ["super-admin", "admin", "owner", "sub-owner", "user"],
+    default: 'user'
+  })
+  role: string;
 
   @Prop()
   age: number;
