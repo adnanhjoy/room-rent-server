02848c6298b396518aa2bd42431c9a29ae686ff6
aotrek <otatbpdevelopment@gmail.com>
2025-05-04 16:52:15 +0600

    [ADD] - added swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
+  const app = await NestFactory.create(AppModule, {
     logger: false
   });
-  // console.log("Listening Port is ", process.env.PORT);
+
+  const config = new DocumentBuilder().setTitle("Room Rent Server").setDescription("Rent Service Management").setVersion('1.0').addTag('rent').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  console.log("Listening Port is ", `http://localhost:${process.env.PORT}`);
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
