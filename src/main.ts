33ae9e559efb6e909bbe76d346491570306ef66a
aotrek <otatbpdevelopment@gmail.com>
2025-05-04 16:17:00 +0600

    [MOD] - modify main file logger hidden

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log("Listening Port is ", process.env.PORT);
+  const app = await NestFactory.create(AppModule,{
+    logger: false
+  });
+  // console.log("Listening Port is ", process.env.PORT);
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
