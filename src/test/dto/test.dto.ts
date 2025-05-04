5960d97eaf52bcb6ad5f958761a7a50705c06d78
aotrek <otatbpdevelopment@gmail.com>
2025-05-04 16:52:15 +0600

    [ADD] - added swagger added

--- src/test/dto/test.dto.ts
+++ src/test/dto/test.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateTestDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({required:false})
   @IsOptional()
   @IsNumber()
   age?: number;
