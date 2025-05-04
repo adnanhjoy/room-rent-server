db15c7b04367ae7e01e75f38a37196ab6a4cf03b
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-05-04 15:55:21 +0600

    add error handler

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/user/user.schema';
 import { CreateUserDto } from 'src/user/dto/user.dto';
+import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken';
+import ApiError from 'src/errors/ApiError';
+
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>
-  ) {}
+  ) { }
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
+  async createUser(createUserDto: CreateUserDto): Promise<any> {
+    const { name, email, password } = createUserDto || {};
+
+    try {
+      const isExist = await this.userModel.findOne({ email });
+
+      if (isExist) {
+        throw new ApiError(HttpStatus.FORBIDDEN, "User already exists")
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const createdUser = new this.userModel({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      await createdUser.save();
+
+      const token = jwt.sign({ name, email }, process.env.JWT_SECRET as string, {
+        expiresIn: '1d',
+      });
+
+      return { token };
+
+    } catch (error) {
+      throw new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "There was a server side error")
+    }
   }
+
 }
