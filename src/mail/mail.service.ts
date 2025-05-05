c0164bfcc668ab50ff5021e1f8b1b14968317896
cseswapon <cseswaponsaha@gmail.com>
2025-05-05 23:31:01 +0600

    [ADD] - added forgote password feature

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,6 +5,7 @@ import * as nodemailer from 'nodemailer';
 export class MailService {
   private transporter = nodemailer.createTransport({
     service: 'gmail',
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
