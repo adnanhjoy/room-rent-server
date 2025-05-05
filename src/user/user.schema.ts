0db4a62c811f2540f65b58f77ca1261e60b1b224
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-05-05 11:39:30 +0600

    update user create

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -24,7 +24,7 @@ export class User {
 
   @Prop({
     type: String,
-    enum: ["super-admin", "admin", "owner", "sub-owner", "user"],
+    enum: ["superadmin", "admin", "owner", "subowner", "user"],
     default: 'user'
   })
   role: string;
