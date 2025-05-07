43d2f3a99c599f36318730cd01cfbdde11bee824
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-05-07 13:13:16 +0600

    add apartment module

--- src/apartment/apartment.module.ts
+++ src/apartment/apartment.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ApartmentController } from './apartment.controller';
+import { ApartmentService } from './apartment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Apartment, ApartmentSchema } from './apartment.schema';
 
-@Module({})
-export class ApartmentModule {}
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Apartment.name, schema: ApartmentSchema }]),
+    ],
+    controllers: [ApartmentController],
+    providers: [ApartmentService],
+})
+export class ApartmentModule { }
