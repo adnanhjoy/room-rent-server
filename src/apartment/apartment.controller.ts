ac9bc7ecba2a49ea0e6e6cfdfd2c32a2769c51d4
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-05-10 11:13:53 +0600

    add update method on apartment service

--- src/apartment/apartment.controller.ts
+++ src/apartment/apartment.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Put, Req, Res, UseGuards } from '@nestjs/common';
 import { ApartmentService } from './apartment.service';
 import { sendResponse } from 'src/utils/sendResponse';
 import { ApartmentDto } from './dto/apartment.dto';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { UserGuardGuard } from 'src/common/guard/user.guard/user.guard.guard';
 
 @Controller('apartment')
 export class ApartmentController {
@@ -34,4 +35,35 @@ export class ApartmentController {
             data: result
         })
     }
+
+
+
+    // update apartment 
+    @UseGuards(UserGuardGuard)
+    @Put('/update/:id')
+    async updateApartment(
+        @Body() body: Partial<ApartmentDto>,
+        @Req() req: Request<{ id: string }> & { role?: string; email?: string },
+        @Res() res: Response
+    ) {
+        const role = req.role;
+        const id = req.params.id;
+
+        if (!role) {
+            return res.status(HttpStatus.BAD_REQUEST).json({
+                success: false,
+                message: "Role is missing from token",
+            });
+        }
+
+        const result = await this.apartmentService.updateApartment(role, id, body);
+console.log(result);
+        sendResponse(res, {
+            success: true,
+            statusCode: HttpStatus.OK,
+            message: "Apartment update successful",
+            data: result,
+        });
+    }
+
 }
